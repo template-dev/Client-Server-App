bbbed8acaa1ecba02ecf1b1e21cb249e403e3414
Vladislav <jukov801@gmail.com>
2024-03-05 15:14:33 +0300

    Base Project

---
+++ server.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+int int main(int argc, const char** argv) {
+
+
+
+
+  return 0;
+}
